cc136554610c807b3b1b5dfe89a1e68640df5415
Luke Hayes <lukepurplehayes@gmail.com>
2023-08-12 11:16:26 +0100

    Added Game::Interactable to project.

---
+++ include/Game/Interactable.h
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace Game
+{
+    /**
+    * Base class for all "Interactable" things.
+    */
+    class Interactable
+    {
+    public:
+        Interactable();
+        ~Interactable();
+
+    private:
+
+    };
+
+    Interactable::Interactable()
+    {
+    }
+
+    Interactable::~Interactable()
+    {
+    }
+
+}
