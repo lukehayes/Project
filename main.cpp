3543f76fa27c070b98b05c628385233c0d8036a4
Luke Hayes <lukepurplehayes@gmail.com>
2023-09-07 09:34:04 +0100

    Pickup collision is now working.

--- main.cpp
+++ main.cpp
@@ -3,12 +3,9 @@
 #include "Game/Pickup.h"
 #include "Game/SpecialPickup.h"
 #include "Game/Entity.h"
-#include "Game/Sprite.h"
-#include "raymath.h"
 
 #include <vector>
 #include <memory>
-#include <iostream>
 
 bool GAME_RUNNING = false;
 
@@ -18,33 +15,38 @@ int main() {
     Game::Game game;
 
     std::vector<std::shared_ptr<Game::Entity>> entities;
-    std::vector<std::shared_ptr<Game::Sprite>> sprites;
-    std::vector<std::shared_ptr<Game::Entity>> pickups;
+    std::vector<std::shared_ptr<Game::Pickup>> pickups;
     
     std::shared_ptr<Game::Player> player = std::make_shared<Game::Player>((Vector2){10,10}, 10, BLUE);
     entities.push_back(player);
 
-    std::shared_ptr<Game::Entity> p = std::make_shared<Game::Pickup>((Vector2){300,200}, 30, GREEN);
-    entities.push_back(p);
+    std::shared_ptr<Game::Pickup> p = std::make_shared<Game::Pickup>((Vector2){170,200}, 30, GREEN);
+    pickups.push_back(p);
 
-    std::shared_ptr<Game::Entity> sp = std::make_shared<Game::SpecialPickup>((Vector2){100,300}, 50, PURPLE);
-    entities.push_back(sp);
+    std::shared_ptr<Game::Pickup> sp = std::make_shared<Game::SpecialPickup>((Vector2){100,130}, 50, PURPLE);
+    pickups.push_back(sp);
 
 
     while(!GAME_RUNNING)
     {
         if (IsKeyPressed(KEY_ESCAPE) || WindowShouldClose()) GAME_RUNNING = true;
 
+        // Entity updates - mostly the player.
         for(auto& entity : entities)
         {
             entity->update(GetFrameTime());
+        }
+
+        // Pickup updates.
+        // Check for pickup collisions>
+        for(auto& pickup : pickups)
+        {
+            TraceLog(LOG_DEBUG, "Check Collision");
 
             // Collisions
-            if(CheckCollisionRecs(player->getShape(), entity->getShape()))
+            if(CheckCollisionRecs(player->getShape(), pickup->getShape()))
             {
-                auto pickup = static_cast<Game::Pickup*>(entity.get());
-
-                //std::cout << pickup->getName() << std::endl;
+                TraceLog(LOG_DEBUG, pickup->getName().c_str());
             }
         }
 
@@ -56,6 +58,10 @@ int main() {
                 entity->render();
             }
 
+            for(auto pickup : pickups)
+            {
+                pickup->render();
+            }
         EndDrawing();
     }
 
