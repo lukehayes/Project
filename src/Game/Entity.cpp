455c693069955c4a4cc230fef91b1e53bda88f60
Luke Hayes <lukepurplehayes@gmail.com>
2023-09-10 09:46:55 +0100

    Entity shape is set in ctor initializer.

--- src/Game/Entity.cpp
+++ src/Game/Entity.cpp
@@ -3,17 +3,10 @@
 namespace Game
 {
     Entity::Entity(const Vector2& position, float scale, const Color color)
-        : position(position), scale(scale), color(color)
+        : position(position), scale(scale), color(color), 
+          shape({position.x, position.y, scale, scale})
     {
         TraceLog(LOG_DEBUG, "Entity Created");
-        
-        // TODO Set the shape explicitly here. Not great but works for now.
-        this->setShape({
-            this->position.x,
-            this->position.y,
-            scale,
-            scale
-        });
     }
 
     Entity::~Entity()
@@ -38,6 +31,8 @@ namespace Game
     void Entity::setPosition(const Vector2& position)
     {
         this->position = position;
+        this->shape.x = position.x;
+        this->shape.y = position.y;
     }
 
     Vector2 Entity::getPosition() const
